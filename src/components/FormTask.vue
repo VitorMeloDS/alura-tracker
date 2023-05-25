c566126f9f3463e7cc1f35c18fad2d79fad757e2
vitor melo <vmsvitor20@gmail.com>
2023-05-24 23:34:43 -0300

    feat: create component Timer

--- src/components/FormTask.vue
+++ src/components/FormTask.vue
@@ -5,21 +5,7 @@
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
       </div>
       <div class="column">
-        <div class="forms">
-          <Cronometro :tempSegunds="tempSegunds"/>
-          <button class="button" @click="started" :disabled="play">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finished">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Timer/>
       </div>
     </div>
   </div>
@@ -27,34 +13,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Cronometro from './Cronometro.vue';
+import Timer from './Timer.vue';
 
 export default defineComponent({
   name: 'FormTask',
-  data() {
-    return {
-      tempSegunds: 0,
-      cronometro: 0,
-      play: false
-    }
-  },
-  methods: {
-    started() {
-      if (this.tempSegunds == 0) {
-        this.play = true;
-        this.cronometro = setInterval(() => {
-          ++this.tempSegunds;
-        }, 1000);
-      }
-    },
-    finished() {
-      this.play = false;
-      this.tempSegunds = 0;
-      clearInterval(this.cronometro);
-    }
-  },
+
   components: {
-    Cronometro,
+    Timer,
   }
 })
 </script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="forms">
+    <Cronometro :tempSegunds="tempSegunds"/>
+      <button class="button" @click="started" :disabled="play">
+        <span class="icon">
+          <i class="fas fa-play"></i>
+        </span>
+        <span>play</span>
+      </button>
+      <button class="button" @click="finished" :disabled="!play">
+        <span class="icon">
+          <i class="fas fa-stop"></i>
+        </span>
+        <span>stop</span>
+      </button>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Timer',
+  data() {
+    return {
+      tempSegunds: 0,
+      cronometro: 0,
+      play: false
+    }
+  },
+  methods: {
+    started() {
+      if (this.tempSegunds == 0) {
+        this.play = true;
+        this.cronometro = setInterval(() => {
+          ++this.tempSegunds;
+        }, 1000);
+      }
+    },
+    finished() {
+      this.play = false;
+      this.tempSegunds = 0;
+      clearInterval(this.cronometro);
+    }
+  },
+  components: {
+    Cronometro,
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
