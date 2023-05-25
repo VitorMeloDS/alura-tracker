f78cafb7c59ed0d1d9e0a225ba663e81d5ceddb0
vitor melo <vmsvitor20@gmail.com>
2023-05-24 23:10:35 -0300

    feat: create component Cronometro

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <strong>
+      {{ tempoDecorrido }}
+    </strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Cronometro',
+  props: {
+    tempSegunds: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempSegunds * 1000).toISOString().split('T')[1].split('.')[0];
+    }
+  },
+})
+</script>
+
